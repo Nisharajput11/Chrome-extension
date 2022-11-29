e5161e6b73563c9d6782103b4a06091a6bf12668
Nisharajput11 <nisharajput0112@gmail.com>
2022-11-30 03:10:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var el = document.getElementById('mybutton');
+if(el){
+  el.addEventListener('click', chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+}, function(tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
+    
+    alert(tab.title);
+}));
+}
+
+
+
